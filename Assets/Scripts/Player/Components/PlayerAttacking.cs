b2a555f7928905f8e890ea354679cb3c14555f38
Daniil <flyjeray.dev@gmail.com>
2024-05-08 22:18:17 +0300

    feat: revert aiming back to cursor

--- Assets/Scripts/Player/Components/PlayerAttacking.cs
+++ Assets/Scripts/Player/Components/PlayerAttacking.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 [RequireComponent(typeof(PlayerCamera))]
 [RequireComponent(typeof(PlayerMovement))]
 [RequireComponent(typeof(PlayerUI))]
@@ -12,17 +11,11 @@ public class PlayerAttacking : MonoBehaviour
 {
     [SerializeField]
     private bool isAiming;
-    private float aimAngle = 0;
-    private float aimLineWidth = .1f;
-    private float aimSens = 500;
-    private float aimLineEndAlpha = .1f;
-
     private bool maxAmmoOnSpawn = true;
 
     private PlayerCamera playerCamera;
     private PlayerMovement movement;
     private PlayerUI playerUI;
-    private LineRenderer aimLine;
 
     public GameObject weaponPrefab;
     private PlayerWeapon weapon;
@@ -37,21 +30,9 @@ public class PlayerAttacking : MonoBehaviour
         weapon = weapon_go.GetComponent<PlayerWeapon>();
         weapon.BindUI(playerUI);
 
-        aimLine = GetComponent<LineRenderer>();
-        aimLine.enabled = false;
-        aimLine.startWidth = aimLineWidth;
-        aimLine.endWidth = aimLineWidth;
-        aimLine.material = new Material(Shader.Find("Sprites/Default"));
-        aimLine.startColor = Color.red;
-        aimLine.endColor = Color.red;
-        aimLine.sortingOrder = 4;
-
         if (maxAmmoOnSpawn && weapon) {
             weapon.ReloadInstantly();
         }
-
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Start() {
@@ -73,41 +54,23 @@ public class PlayerAttacking : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R) && weapon && !weapon.IsReloading() && weapon.IsReloadNeeded()) {
             weapon.Reload();
         } else if (isAiming && weapon) {
-            float resultAngle = movement.IsFacingRight() ? -aimAngle : aimAngle;
-            
-            Vector3 aimLineStart = transform.position;
-            if (weapon.transform.childCount > 0) {
-                aimLineStart = weapon.transform.GetChild(0).position;
-            }
-
-            Vector3 aimLineDir = Quaternion.Euler(0, 0, resultAngle) * Vector3.up;
+            Vector3 cursorPointOnScreen = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             weapon.FaceSpriteRight(movement.IsFacingRight());
-            weapon.transform.rotation = Quaternion.Euler(0, 0, resultAngle + 90);
-
-            RaycastHit2D hit = Physics2D.Raycast(aimLineStart, aimLineDir, weapon.GetMaxDistance(), ~LayerMask.GetMask("Player", "Ignore Raycast"));
-
-            if (hit.collider && hit.collider.GetComponent<HealthManager>()) {
-                aimLine.startColor = Color.red;
-                aimLine.endColor = new Color{ a=aimLineEndAlpha, r=255 };
-            } else {
-                aimLine.startColor = Color.green;
-                aimLine.endColor = new Color{ a=aimLineEndAlpha, g=255 };
-            }
-            
-            aimLine.SetPositions(new Vector3[]{ aimLineStart, hit.collider ? hit.point : aimLineStart + aimLineDir * weapon.GetMaxDistance() });
+            weapon.transform.LookAt(new Vector3(
+                cursorPointOnScreen.x,
+                cursorPointOnScreen.y,
+                weapon.transform.position.z
+            ));
 
             if (Input.GetMouseButton(0) && weapon.IsAbleToShoot()) {
-                weapon.Shoot(aimLineStart + aimLineDir * weapon.GetMaxDistance(), movement.GetCollider2D());
+                weapon.Shoot(cursorPointOnScreen, movement.GetCollider2D());
                 StartCoroutine(ShakeCamera(weapon.GetCameraShakeIntensity(), 0.2f));
             }
-
-            aimAngle = Math.Clamp(aimAngle - Input.GetAxis("Mouse Y") * Time.fixedDeltaTime * aimSens, 0, 180);
         }
         
         if (weapon) {
             weapon.SwitchVisibility(isAiming);
-            aimLine.enabled = isAiming;
         }
     }
 
